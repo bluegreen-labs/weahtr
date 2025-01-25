50525789f35eebff73486464029c889d28c50295
Koen Hufkens <koen.hufkens@gmail.com>
2025-01-25 19:14:02 +0100

    test script cell subsets and line removal

--- analysis/test_grid_removal.py
+++ analysis/test_grid_removal.py
@@ -8,19 +8,10 @@ base = "/data/"
 
 # list files to process
 images =  glob.glob(os.path.join(base, "data/character_training_data/images/*.png"))
-images =  glob.glob(os.path.join(base, "data/format_1/*.jpg"))
+#images =  glob.glob(os.path.join(base, "data/format_1/*.jpg"))
 images = images[2]
 
 image = cv2.imread(images)
-image = remove_lines(image)
-cv2.imwrite("demo.png", image)
-
-# figure, ax = plt.subplots(nrows=len(images)//2, ncols=2, figsize=(12, 6))
-# for i in range(len(images)):
-#     image = images[i]
-#     image = remove_lines(image)
-#     ax.ravel()[i].imshow(image)
-#     ax.ravel()[i].set_axis_off()
-# plt.tight_layout()
-# plt.savefig('demo.png')
-
+crop = subset_cell(image)
+crop = remove_lines(crop)
+cv2.imwrite("demo.png", crop)
