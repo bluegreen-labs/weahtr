b780dcf0e9edee58510efc62444309489c81315d
Koen Hufkens <koen.hufkens@gmail.com>
2025-01-29 18:04:24 +0100

    image transformation test script + data

---
+++ demo/random/image_transformation.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# import libraries
+import glob, os
+from weahtr import * 
+from weahtr.utils import *
+from weahtr.transform import *
+
+# set base directory
+base = "/data/"
+
+# list files to process
+images =  glob.glob(
+  os.path.join(
+    base,
+    "demo/random/images/*.png"
+    )
+  )
+
+# define custom function
+def custom_function(image):
+  image = remove_lines(image, mean_fill = True)
+  image = binarize(image, window_size = 101, C = 15)
+  return(image)
+
+# convert and write to disk
+image = cv2.imread(images[2])
+
+# custom function
+image_custom = custom_function(image)
+cv2.imwrite("custom_function.png", image_custom)
+
+# convert and write to disk
+image = cv2.imread(images[1])
+
+# use transformation function
+image_transform = train_transform(image = image)['image']
+cv2.imwrite("transform.png", image_transform)
